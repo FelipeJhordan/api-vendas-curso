16cccb24b13f6cd9c39af8001b2fbb78f131c686
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-05 12:42:28 -0300

    feat(modules/product): Create service for adding product in the database

---
+++ src/modules/modules/products/typeorm/services/CreateProductService.ts
@@ -0,0 +1,31 @@
+import { ProductRepository } from './../repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import Product from '../entities/Product';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
