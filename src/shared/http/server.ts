71ba45b908dbe30a60aa262bd0631152b0a16eaf
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-13 16:10:30 -0300

    feat: create singles midleware with the package celebrate for validate inputs by users

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import interceptorError from './interceptors/error';
 import '@shared/typeorm';
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(errors());
 
 app.use(interceptorError);
 
