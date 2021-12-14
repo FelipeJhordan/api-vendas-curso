20e0f6a467a62cabc922aab1a7d0faf75e6733b0
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-14 13:09:15 -0300

    feat: add use controller routes to main url indicators

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productsRouter from '@modules/modules/products/routes/products.routes';
+import usersRouter from '@modules/modules/users/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/users', usersRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Dev!' });
