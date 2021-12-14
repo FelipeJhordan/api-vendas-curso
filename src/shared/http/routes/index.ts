ede5073a7b38e323a74a5c1b45ac7e85103b2f8c
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-14 16:29:05 -0300

    feat: Link session routes with app

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import productsRouter from '@modules/modules/products/routes/products.routes';
 import usersRouter from '@modules/modules/users/routes/users.routes';
+import sessionsRouter from '@modules/modules/users/routes/sesssions.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Dev!' });
