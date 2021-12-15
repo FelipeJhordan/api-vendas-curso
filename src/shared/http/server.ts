81a2c70a184879c75eef881265e7f7d1d89e3843
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-15 13:54:38 -0300

    feat(server): add static route for access avatars

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,12 +6,14 @@ import { errors } from 'celebrate';
 import routes from './routes';
 import interceptorError from './interceptors/error';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
-
 app.use(errors());
 
 app.use(interceptorError);
