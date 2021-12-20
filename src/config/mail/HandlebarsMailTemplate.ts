822cf43260a1bcdbdc6e16270514f96ff237b98e
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-19 22:38:24 -0300

    chore: Create configuration to read template and link variables to template by handlebars

---
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -0,0 +1,13 @@
+import handlebars from 'handlebars';
+import IParseMailTemplate from './IParseMailTemplate';
+import fs from 'fs';
+
+export default class handlebarsMailTemplate {
+  public async parse({ file, variables }: IParseMailTemplate): Promise<string> {
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+    const parseTemplate = handlebars.compile(templateFileContent);
+    return parseTemplate(variables);
+  }
+}
