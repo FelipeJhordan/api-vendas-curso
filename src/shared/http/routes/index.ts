00334cc9f985e8ee7b141153e5b8765a33d72c54
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-19 22:46:44 -0300

    feat: Link password  recover routes with app

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import productsRouter from '@modules/modules/products/routes/products.routes';
 import usersRouter from '@modules/modules/users/routes/users.routes';
 import sessionsRouter from '@modules/modules/users/routes/sesssions.routes';
-
+import passwordRouter from '@modules/modules/users/routes/password.routes';
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Dev!' });
