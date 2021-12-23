ca704bbe88206b99c1e02f524e03137e8d881a9a
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-23 16:45:14 -0300

    feat: link profile routes with remainder routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,12 +3,14 @@ import productsRouter from '@modules/modules/products/routes/products.routes';
 import usersRouter from '@modules/modules/users/routes/users.routes';
 import sessionsRouter from '@modules/modules/users/routes/sesssions.routes';
 import passwordRouter from '@modules/modules/users/routes/password.routes';
+import profileRouter from '@modules/modules/users/routes/profile.routes';
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Dev!' });
