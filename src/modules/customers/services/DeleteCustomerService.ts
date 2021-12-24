f7d1ac7bc2eeb2beaaee0d1401bc4812c36ade04
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-24 13:12:22 -0300

    feat(module/customers): Create service DeleteCustomerService

---
+++ src/modules/customers/services/DeleteCustomerService.ts
@@ -0,0 +1,20 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import CustomerRepository from '../typeorm/repositories/CustomerRepository';
+
+interface IRequest {
+  customer_id: string;
+}
+class DeleteCustomerService {
+  public async execute({ customer_id }: IRequest): Promise<void> {
+    const customersRepository = getCustomRepository(CustomerRepository);
+
+    const customer = await customersRepository.findById(customer_id);
+
+    if (!customer) throw new AppError('Customer not found.');
+
+    await customersRepository.remove(customer);
+  }
+}
+
+export default DeleteCustomerService;
