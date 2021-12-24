1079d1ebc43121e685ad8b75eed95cf2d4dcc8cd
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-24 11:40:49 -0300

    fix: fix importations into main file routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import productsRouter from '@modules/modules/products/routes/products.routes';
-import usersRouter from '@modules/modules/users/routes/users.routes';
-import sessionsRouter from '@modules/modules/users/routes/sesssions.routes';
-import passwordRouter from '@modules/modules/users/routes/password.routes';
-import profileRouter from '@modules/modules/users/routes/profile.routes';
+import productsRouter from '@modules/products/routes/products.routes';
+import usersRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sesssions.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 const routes = Router();
 
 routes.use('/products', productsRouter);
