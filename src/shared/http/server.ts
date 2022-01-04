4e56aaddcdfe3112b0bdf155441349a586643da9
Felipe Jhordan <felipejordan.alves@gmail.com>
2022-01-03 22:32:11 -0300

    feat: update server.ts and use pagination midleware by 'typeorm-pagination' lib

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
 import routes from './routes';
 import interceptorError from './interceptors/error';
+import rateLimiter from './interceptors/rateLimiter';
 import '@shared/typeorm';
 import uploadConfig from '@config/upload';
 
@@ -12,6 +15,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter);
+app.use(pagination);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
