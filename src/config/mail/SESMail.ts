371678356268d8c5a3a6dd971e2b0f5640b53988
Felipe Jhordan <felipejordan.alves@gmail.com>
2022-01-04 12:24:59 -0300

    feat/refactor: configure SESMail class and refactor mail configuration file

---
+++ src/config/mail/SESMail.ts
@@ -0,0 +1,48 @@
+import nodemailer from 'nodemailer';
+import IParseMailTemplate from './IParseMailTemplate';
+import aws from 'aws-sdk';
+import HandlebarsMailtemplate from './HandlebarsMailTemplate';
+import mailConfig from '@config/mail/mail';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+interface ISendMail {
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
+}
+
+export default class SESMail {
+  static async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMail): Promise<void> {
+    const mailTemplate = new HandlebarsMailtemplate();
+
+    const transporter = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+      }),
+    });
+
+    const { email, name } = mailConfig.defaults.from;
+
+    const message = await transporter.sendMail({
+      from: {
+        name: from?.name || name,
+        address: from?.email || email,
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parse(templateData),
+    });
+  }
+}
