2238c5012085bbbacb8660e19d7a7968a8b4b41b
Felipe Jhordan <felipejordan.alves@gmail.com>
2022-01-04 12:27:38 -0300

    refactor: refactor upload config

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,19 +1,43 @@
 import path from 'path';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
+
+interface IUploadConfig {
+  directory: string;
+  tmpFolder: string;
+  driver: 's3' | 'disk';
+  multer: { storage: StorageEngine };
+  config: {
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
   directory: uploadFolder,
-  storage: multer.diskStorage({
-    destination: uploadFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const filename = `${fileHash}-${new Date().getMilliseconds()}-${
-        file.originalname
-      }`;
+  tmpFolder,
+  driver: process.env.STORAGE_DRIVER,
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const filename = `${fileHash}-${new Date().getMilliseconds()}-${
+          file.originalname
+        }`;
 
-      callback(null, filename);
+        callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'api-vendas-fjalves',
     },
-  }),
-};
+  },
+} as IUploadConfig;
